fb164b22b3677cee90aa087d3ea7cb56c9d4932e
Jorge Amil Perez Ponsoda <japp7@alu.ua.es>
2020-05-20 19:46:51 +0200

    refactorizacion filtro VG, SOR, normales, puntos y caracteristicas

--- src/send_velocity_commands/main.cpp
+++ src/send_velocity_commands/main.cpp
@@ -12,79 +12,81 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/filters/voxel_grid.h>
 
-class RobotDriver
-{
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/keypoints/iss_3d.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <pcl/features/fpfh.h>
+#include <pcl/kdtree/kdtree.h>
+
+class RobotDriver {
 private:
-  //! The node handle we'll be using
   ros::NodeHandle nh_;
   ros::NodeHandle nh_stamp_;
-  //! We will be publishing to the "/base_controller/command" topic to issue commands
+
   ros::Publisher cmd_stamp_pub_;
   ros::Publisher cmd_pub_;
-  // teclas
-  char teclas_ [4];
+
+  char teclas_ [5];
 
 public:
-  //! ROS node initialization
-  RobotDriver(ros::NodeHandle &nh)
-  {
+  RobotDriver(ros::NodeHandle &nh) {
     nh_ = nh;
-    //set up the publisher for the cmd_vel topic
-    ///robot1/mobile_base/commands/velocity
+
     cmd_stamp_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("chatter", 1);
     cmd_pub_ = nh_stamp_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+
     teclas_[0] = 'w'; // delante
     teclas_[1] = 'a'; // izquierda
     teclas_[2] = 'd'; // derecha
     teclas_[3] = 'p'; // parar bucle infinito
+    teclas_[4] = 'r'; // parar bucle infinito
   }
 
-  //! Loop forever while sending drive commands based on keyboard input
-  bool driveKeyboard()
-  {
+  bool driveKeyboard() {
     std::cout << 
       "forward: " << teclas_[0] << 
       " left: "   << teclas_[1] <<
       " right: "  << teclas_[2] <<
-      " exit: "   << teclas_[3] << "\n";
+      " exit: "   << teclas_[3] <<
+      " capture: "<< teclas_[4] << "\n";
 
-    //sending commands type
     geometry_msgs::TwistStamped base_stamp_cmd;  
     geometry_msgs::Twist base_cmd;
 
     char cmd[50];
-    while(nh_.ok() && nh_stamp_.ok()){
 
+    while(nh_.ok() && nh_stamp_.ok()) {
       std::cin.getline(cmd, 50);
-      if(cmd[0]!=teclas_[0] && cmd[0]!=teclas_[1] && cmd[0]!=teclas_[2] && cmd[0]!=teclas_[3] && cmd[0]!=teclas_[4])
-      {
+      if (cmd[0] != teclas_[0] && cmd[0] != teclas_[1] && cmd[0] != teclas_[2] && cmd[0] != teclas_[3] && cmd[0] != teclas_[4]) {
         std::cout << "unknown command:" << cmd << "\n";
         continue;
       }
+
       base_stamp_cmd.twist.linear.x = base_stamp_cmd.twist.linear.y = base_stamp_cmd.twist.angular.z = 0;
       base_stamp_cmd.header.stamp = ros::Time::now();
       base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;   
     
-      //move forward
-      if(cmd[0]==teclas_[0]){
+      // delante
+      if (cmd[0] == teclas_[0]) {
         base_stamp_cmd.twist.linear.x = base_cmd.linear.x = 0.25;
       } 
-      //turn left (yaw) and drive forward at the same time
-      else if(cmd[0]==teclas_[1]){
+      // izquierda
+      else if (cmd[0] == teclas_[1]) {
         base_stamp_cmd.twist.angular.z = base_cmd.angular.z = 0.5;
       } 
-      //turn right (yaw) and drive forward at the same time
-      else if(cmd[0]==teclas_[2]){
+      // derecha
+      else if (cmd[0] == teclas_[2]) {
         base_stamp_cmd.twist.angular.z = base_cmd.angular.z = -0.5;
       } 
-      //quit
-      else if(cmd[0]==teclas_[3]){
+      // terminar
+      else if (cmd[0] == teclas_[3]) {
         break;
       }
+      // capturar
+      else if (cmd[0] == teclas_[4]) {
+  	    ros::spinOnce();
+      }
 
-	    ros::spinOnce();
-
-      //publish the assembled command
       std::cout << 
         "x: " << base_stamp_cmd.twist.linear.x  << 
         " y: " << base_stamp_cmd.twist.linear.y  <<
@@ -94,29 +96,130 @@ public:
       cmd_stamp_pub_.publish(base_stamp_cmd);
       cmd_pub_.publish(base_cmd);
     }
+
     return true;
   }
 };
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+bool capturaInicial = true;
 
-void simpleVis ()
-{
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+pcl::PointCloud<pcl::PointWithScale>::Ptr puntosCaracteristicos (new pcl::PointCloud<pcl::PointWithScale> ());
+pcl::PointCloud<pcl::FPFHSignature33>::Ptr fpfhs (new pcl::PointCloud<pcl::FPFHSignature33> ());
+
+void simpleVis () {
 	pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-	while(!viewer.wasStopped())
-	{
+	while(!viewer.wasStopped()) {
 	  viewer.showCloud (visu_pc);
 	  boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 	}
 }
 
-void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
-{
+void filtroVoxelGrid(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& nube, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& nube_VG){
+  pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
+
+  vGrid.setInputCloud (nube);
+  vGrid.setLeafSize (0.025f, 0.025f, 0.025f);
+  vGrid.filter (*nube_VG);
+}
+
+void filtroSor(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& nube_VG, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& nube_SOR){
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> filtro_SOR;
+
+  filtro_SOR.setInputCloud (nube_VG);
+
+  // Probar parametros http://wiki.ros.org/pcl_ros/Tutorials/filters
+  filtro_SOR.setMeanK (100);
+  filtro_SOR.setStddevMulThresh (0.25);
+
+  filtro_SOR.filter (*nube_SOR);
+}
+
+void estimacionNormales(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& nube_SOR, pcl::PointCloud<pcl::PointNormal>::Ptr& nube_normales) {
+  pcl::NormalEstimation<pcl::PointXYZRGB, pcl::PointNormal> estimacionNormal;
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr arbol_normales(new pcl::search::KdTree<pcl::PointXYZRGB>());
+
+  estimacionNormal.setInputCloud(nube_SOR);
+  estimacionNormal.setSearchMethod(arbol_normales);
+
+  // http://docs.ros.org/indigo/api/pcl_ros/html/classpcl__ros_1_1NormalEstimation.html
+  estimacionNormal.setRadiusSearch(0.05);
+
+  estimacionNormal.compute(*nube_normales);
+}
+
+void extraccionPuntosCaracteristicos(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& nube_SOR, pcl::PointCloud<pcl::PointNormal>::Ptr& nube_normales) {
+  for(size_t i = 0; i < nube_normales->points.size(); ++i) {
+    nube_normales->points[i].x = nube_SOR->points[i].x;
+    nube_normales->points[i].y = nube_SOR->points[i].y;
+    nube_normales->points[i].z = nube_SOR->points[i].z;
+  }
+
+  pcl::SIFTKeypoint<pcl::PointNormal, pcl::PointWithScale> sift;
+
+  const static float escalaMinima = 0.01f;
+  const static int numeroOctavas = 3;
+  const static int escalasPorOctavas = 4;
+  const static float contrasteMinimmo = 0.001f;
+
+  pcl::search::KdTree<pcl::PointNormal>::Ptr arbol(new pcl::search::KdTree<pcl::PointNormal> ());
+
+  sift.setSearchMethod(arbol);
+  sift.setScales(escalaMinima, numeroOctavas, escalasPorOctavas);
+  sift.setMinimumContrast(contrasteMinimmo);
+  sift.setInputCloud(nube_normales);
+  sift.compute(*puntosCaracteristicos);
+}
+
+void extraccionCaracteristicas(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud, pcl::PointCloud<pcl::PointNormal>::Ptr& cloud_normals, pcl::PointCloud<pcl::FPFHSignature33>::Ptr& fpfhs){
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr keypoints_xyzrgb(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+  pcl::copyPointCloud (*puntosCaracteristicos, *keypoints_xyzrgb);
+
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB> ());
+  pcl::FPFHEstimation<pcl::PointXYZRGB, pcl::PointNormal, pcl::FPFHSignature33> fpfh;
+
+  // http://docs.ros.org/indigo/api/pcl_ros/html/classpcl__ros_1_1FPFHEstimation.html
+  fpfh.setInputCloud (keypoints_xyzrgb);
+  fpfh.setInputNormals (cloud_normals);
+  fpfh.setSearchSurface (cloud);
+  fpfh.setSearchMethod(tree);
+  fpfh.setRadiusSearch (0.07);
+  fpfh.compute (*fpfhs);
+}
+
+void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg) {
+  if (capturaInicial) {
+//    *visu_pc = *msg;
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr nube (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr nube_VG (new pcl::PointCloud<pcl::PointXYZRGB>);
+    filtroVoxelGrid(nube, nube_VG);
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr nube_SOR (new pcl::PointCloud<pcl::PointXYZRGB>);
+    filtroSor(nube_VG, nube_SOR);
+
+    pcl::PointCloud<pcl::PointNormal>::Ptr nube_normales (new pcl::PointCloud<pcl::PointNormal>);
+    estimacionNormales(nube_SOR, nube_normales);
+
+    extraccionPuntosCaracteristicos(nube_SOR, nube_normales);
+    extraccionCaracteristicas(nube_SOR, nube_normales, fpfhs);
+
+    capturaInicial = false;
+    visu_pc = nube_SOR;
+  }
+  else {
+
+  }
+
+  /*
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
 
 	cout << "Puntos capturados: " << cloud->size() << endl;
 
+  // Filtramos los puntos y aplicamos VoxelGrid
 	pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
 	vGrid.setInputCloud (cloud);
 	vGrid.setLeafSize (0.05f, 0.05f, 0.05f);
@@ -125,18 +228,17 @@ void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
 	cout << "Puntos tras VG: " << cloud_filtered->size() << endl;
 
 	visu_pc = cloud_filtered;
+  */
 }
 
-int main(int argc, char** argv)
-{
-  //init the ROS node
+int main(int argc, char** argv) {
   ros::init(argc, argv, "robot_driver");
   ros::NodeHandle nh;
   RobotDriver driver(nh);
 
-	ros::NodeHandle nhCloud;
- 	ros::Subscriber imageSub = nhCloud.subscribe<pcl::PointCloud<pcl::PointXYZRGB> >("/camera/depth/points", 1, callback);
-	boost::thread t(simpleVis);
+  ros::NodeHandle nhCloud;
+  ros::Subscriber imageSub = nhCloud.subscribe<pcl::PointCloud<pcl::PointXYZRGB> >("/camera/depth/points", 1, callback);
+  boost::thread t(simpleVis);
 
   driver.driveKeyboard();
 }
