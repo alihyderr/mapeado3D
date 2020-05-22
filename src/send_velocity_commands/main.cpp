06d6628b1ca7130c0358d0972eb180af431ab40d
Jorge Amil Perez Ponsoda <japp7@alu.ua.es>
2020-05-22 14:46:33 +0200

    initial commit

--- src/send_velocity_commands/main.cpp
+++ src/send_velocity_commands/main.cpp
@@ -5,6 +5,12 @@
 #include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/Twist.h>
 
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/foreach.hpp>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/voxel_grid.h>
 
 class RobotDriver
 {
@@ -16,7 +22,7 @@ private:
   ros::Publisher cmd_stamp_pub_;
   ros::Publisher cmd_pub_;
   // teclas
-  char teclas_ [4];
+  char teclas_ [5];
 
 public:
   //! ROS node initialization
@@ -24,13 +30,13 @@ public:
   {
     nh_ = nh;
     //set up the publisher for the cmd_vel topic
-    ///robot1/mobile_base/commands/velocity
     cmd_stamp_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("chatter", 1);
-    cmd_pub_ = nh_stamp_.advertise<geometry_msgs::Twist>("/robot1/mobile_base/commands/velocity", 1);
+    cmd_pub_ = nh_stamp_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
     teclas_[0] = 'w'; // delante
     teclas_[1] = 'a'; // izquierda
     teclas_[2] = 'd'; // derecha
     teclas_[3] = 'p'; // parar bucle infinito
+    teclas_[4] = 'r'; // obtener nube de puntos
   }
 
   //! Loop forever while sending drive commands based on keyboard input
@@ -40,7 +46,8 @@ public:
       "forward: " << teclas_[0] << 
       " left: "   << teclas_[1] <<
       " right: "  << teclas_[2] <<
-      " exit: "   << teclas_[3] << "\n";
+      " exit: "   << teclas_[3] <<
+      " capture: "<< teclas_[4] << "\n";
 
     //sending commands type
     geometry_msgs::TwistStamped base_stamp_cmd;  
@@ -50,8 +57,7 @@ public:
     while(nh_.ok() && nh_stamp_.ok()){
 
       std::cin.getline(cmd, 50);
-      if(cmd[0]!=teclas_[0] && cmd[0]!=teclas_[1] && cmd[0]!=teclas_[2] && cmd[0]!=teclas_[3] && cmd[0]!=teclas_[4])
-      {
+      if (cmd[0] != teclas_[0] && cmd[0] != teclas_[1] && cmd[0] != teclas_[2] && cmd[0] != teclas_[3] && cmd[0] != teclas_[4]) {
         std::cout << "unknown command:" << cmd << "\n";
         continue;
       }
@@ -65,17 +71,19 @@ public:
       } 
       //turn left (yaw) and drive forward at the same time
       else if(cmd[0]==teclas_[1]){
-        base_stamp_cmd.twist.angular.z = base_cmd.angular.z = 0.75;
-        base_stamp_cmd.twist.linear.x = base_cmd.linear.x = 0.25;
+        base_stamp_cmd.twist.angular.z = base_cmd.angular.z = 0.35;
       } 
       //turn right (yaw) and drive forward at the same time
       else if(cmd[0]==teclas_[2]){
-        base_stamp_cmd.twist.angular.z = base_cmd.angular.z = -0.75;
-        base_stamp_cmd.twist.linear.x = base_cmd.linear.x = 0.25;
+        base_stamp_cmd.twist.angular.z = base_cmd.angular.z = -0.35;
       } 
       //quit
       else if(cmd[0]==teclas_[3]){
         break;
+      }
+      // capturar
+      else if (cmd[0] == teclas_[4]) {
+  	    ros::spinOnce();
       }
 
       //publish the assembled command
@@ -92,12 +100,43 @@ public:
   }
 };
 
-int main(int argc, char** argv)
-{
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+void simpleVis () {
+  pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+  while (!viewer.wasStopped()) {
+    viewer.showCloud (visu_pc);
+    boost::this_thread::sleep (boost::posix_time::milliseconds(1000));
+  }
+}
+
+void callback (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg) {
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
+
+  cout << "Puntos capturados: " << cloud->size() << endl;
+
+  pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
+
+  vGrid.setInputCloud (cloud);
+  vGrid.setLeafSize (0.05f, 0.05f, 0.05f);
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
+  vGrid.filter (*cloud_filtered);
+
+  cout << "Puntos tras VG: " << cloud_filtered->size() << endl;
+
+  visu_pc = cloud_filtered;
+}
+
+int main (int argc, char** argv) {
   //init the ROS node
   ros::init(argc, argv, "robot_driver");
   ros::NodeHandle nh;
+  RobotDriver driver (nh);
 
-  RobotDriver driver(nh);
-  driver.driveKeyboard();
+  ros::NodeHandle nhCloud;
+  ros::Subscriber imageSub = nhCloud.subscribe <pcl::PointCloud<pcl::PointXYZRGB> > ("/camera/depth/points", 1, callback);
+  boost::thread t (simpleVis);
+
+  driver.driveKeyboard ();
 }
