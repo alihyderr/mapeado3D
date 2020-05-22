1ab174d4170e73090d11954c374719b273e94411
Jorge Amil Perez Ponsoda <japp7@alu.ua.es>
2020-05-22 16:28:58 +0200

    captura con send

--- src/send_velocity_commands/main.cpp
+++ src/send_velocity_commands/main.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
 #include <ros/ros.h>
-
 #include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/Twist.h>
 
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
+
 #include <boost/foreach.hpp>
+
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include <pcl/filters/voxel_grid.h>
 
-class RobotDriver
-{
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+
+class RobotDriver {
 private:
   //! The node handle we'll be using
   ros::NodeHandle nh_;
   ros::NodeHandle nh_stamp_;
+
   //! We will be publishing to the "/base_controller/command" topic to issue commands
   ros::Publisher cmd_stamp_pub_;
   ros::Publisher cmd_pub_;
+
   // teclas
   char teclas_ [5];
 
 public:
   //! ROS node initialization
-  RobotDriver(ros::NodeHandle &nh)
-  {
+  RobotDriver (ros::NodeHandle &nh) {
     nh_ = nh;
+
     //set up the publisher for the cmd_vel topic
     cmd_stamp_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("chatter", 1);
-    cmd_pub_ = nh_stamp_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+    cmd_pub_ = nh_stamp_.advertise<geometry_msgs::Twist> ("/mobile_base/commands/velocity", 1);
+
     teclas_[0] = 'w'; // delante
     teclas_[1] = 'a'; // izquierda
     teclas_[2] = 'd'; // derecha
@@ -40,8 +48,7 @@ public:
   }
 
   //! Loop forever while sending drive commands based on keyboard input
-  bool driveKeyboard()
-  {
+  bool driveKeyboard() {
     std::cout << 
       "forward: " << teclas_[0] << 
       " left: "   << teclas_[1] <<
@@ -54,31 +61,33 @@ public:
     geometry_msgs::Twist base_cmd;
 
     char cmd[50];
-    while(nh_.ok() && nh_stamp_.ok()){
 
+    while(nh_.ok() && nh_stamp_.ok()) {
       std::cin.getline(cmd, 50);
+
       if (cmd[0] != teclas_[0] && cmd[0] != teclas_[1] && cmd[0] != teclas_[2] && cmd[0] != teclas_[3] && cmd[0] != teclas_[4]) {
         std::cout << "unknown command:" << cmd << "\n";
         continue;
       }
+      
       base_stamp_cmd.twist.linear.x = base_stamp_cmd.twist.linear.y = base_stamp_cmd.twist.angular.z = 0;
       base_stamp_cmd.header.stamp = ros::Time::now();
       base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;   
     
       //move forward
-      if(cmd[0]==teclas_[0]){
+      if (cmd[0] == teclas_[0]) {
         base_stamp_cmd.twist.linear.x = base_cmd.linear.x = 0.25;
       } 
       //turn left (yaw) and drive forward at the same time
-      else if(cmd[0]==teclas_[1]){
+      else if (cmd[0] == teclas_[1]) {
         base_stamp_cmd.twist.angular.z = base_cmd.angular.z = 0.35;
       } 
       //turn right (yaw) and drive forward at the same time
-      else if(cmd[0]==teclas_[2]){
+      else if (cmd[0] == teclas_[2]) {
         base_stamp_cmd.twist.angular.z = base_cmd.angular.z = -0.35;
       } 
       //quit
-      else if(cmd[0]==teclas_[3]){
+      else if (cmd[0] == teclas_[3]) {
         break;
       }
       // capturar
@@ -93,39 +102,62 @@ public:
         " z: " << base_stamp_cmd.twist.angular.z <<
         " stamp: " << base_stamp_cmd.header.stamp << "\n";
 
-      cmd_stamp_pub_.publish(base_stamp_cmd);
-      cmd_pub_.publish(base_cmd);
+      cmd_stamp_pub_.publish (base_stamp_cmd);
+      cmd_pub_.publish (base_cmd);
     }
     return true;
   }
 };
 
+int capturas = 1;
+
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
 
 void simpleVis () {
   pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+
   while (!viewer.wasStopped()) {
     viewer.showCloud (visu_pc);
+
+    char *intStr = itoa(capturas);
+    string numero = string(intStr);
+
+    pcl::io::savePCDFile ("nube" + numero + ".pcd", visu_pc, true);
+
+    capturas++;
     boost::this_thread::sleep (boost::posix_time::milliseconds(1000));
   }
 }
 
 void callback (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg) {
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr nube (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
+/*
+  cout << "Puntos capturados: " << nube->size() << endl;
 
-  cout << "Puntos capturados: " << cloud->size() << endl;
+  pcl::VoxelGrid<pcl::PointXYZRGB > voxelGrid;
 
-  pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
+  voxelGrid.setInputCloud (nube);
+  voxelGrid.setLeafSize (0.05f, 0.05f, 0.05f);
 
-  vGrid.setInputCloud (cloud);
-  vGrid.setLeafSize (0.05f, 0.05f, 0.05f);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr nubeVG (new pcl::PointCloud<pcl::PointXYZRGB>);
+  voxelGrid.filter (*nubeVG);
 
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
-  vGrid.filter (*cloud_filtered);
+  cout << "Puntos tras VG: " << nubeVG->size() << endl;
 
-  cout << "Puntos tras VG: " << cloud_filtered->size() << endl;
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> filtroSOR;
 
-  visu_pc = cloud_filtered;
+  filtroSOR.setInputCloud (nubeVG);
+  filtroSOR.setMeanK (100);
+  filtroSOR.setStddevMulThresh (0.25);
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr nubeSOR (new pcl::PointCloud<pcl::PointXYZRGB>);
+  filtroSOR.filter (*nubeSOR);
+
+  cout << "Puntos tras SOR: " << nubeSOR->size() << endl;
+
+  visu_pc = nubeSOR;
+  */
+  visu_pc = nube;
 }
 
 int main (int argc, char** argv) {
